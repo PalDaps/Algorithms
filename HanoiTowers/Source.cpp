766252a28fe617caeec8b1a458dd2f049ad8c070
Admin <dashko.pavel@bk.ru>
2023-04-05 21:56:48 +0300

    The nice HanoiTowers alghoritm

--- HanoiTowers/Source.cpp
+++ HanoiTowers/Source.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+
+int HanoiTowers(int n)
+{
+	if (n == 1) return 1;
+	return HanoiTowers(n - 1) * 2 + 1;
+}
+
 int main()
 {
+	int n = 0;
+	std::cin >> n;
+	std::cout << HanoiTowers(n);
 	return 0;
 }
